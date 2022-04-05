9bedeb8f5e8b889a9aa43bebc08f2daf7aaf4ab6
kaow_pongsatorn <76607003+0986548200@users.noreply.github.com>
2022-04-05 12:21:13 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,51 +21,10 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
-
-        <v-list-group :value="true" prepend-icon="mdi-cog">
-          <template v-slot:activator>
-            <v-list-item-title>ตั้งค่า</v-list-item-title>
-          </template>
-
-          <v-list-group :value="true" no-action sub-group>
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>Admin</v-list-item-title>
-              </v-list-item-content>
-            </template>
-
-            <v-list-item
-              v-for="(item, i) in admins"
-              :key="i"
-              link
-              :to="item.to"
-            >
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-list-item>
-          </v-list-group>
-
-          <v-list-group no-action sub-group>
-            <template v-slot:activator>
-              <v-list-item-content>
-                <v-list-item-title>User</v-list-item-title>
-              </v-list-item-content>
-            </template>
-
-            <v-list-item v-for="(item, i) in users" :key="i" link :to="item.to">
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
-            </v-list-item>
-          </v-list-group>
-        </v-list-group>
       </v-list>
       <v-list>
         <v-list-item>
-          <v-btn block large color="red" dark>
+          <v-btn block large color="red" dark @click="logout">
             <v-icon left>mdi-logout</v-icon>Logout
           </v-btn>
         </v-list-item>
@@ -90,85 +49,34 @@ export default {
       items: [
         {
           icon: "mdi-monitor-dashboard",
-          title: "ลูกค้า",
-          to: "/customers",
-        },
-        {
-          icon: "mdi-monitor-dashboard",
-          title: "ซัฟพลายเออร์",
-          to: "/suppliers",
-        },
-        {
-          icon: "mdi-archive-search",
-          title: "รายการสินค้า",
-          to: "/product_list",
-        },{
-          icon: "mdi-archive-search",
-          title: "สินค้า",
-          to: "/stock",
-        },
-        {
-          icon: "mdi-package-down",
-          title: "ใบส่งสินค้าชั่วคราว",
-          to: "/",
-        },
-        {
-          icon: "mdi-package-down",
-          title: "ใบรับเงินชั่วคราว",
-          to: "/",
-        },
-        {
-          icon: "mdi-package-down",
-          title: "ใบส่งสินค้า/บิลเงินสด",
-          to: "/",
-        },
-        {
-          icon: "mdi-package-down",
-          title: "รับเข้า",
-          to: "/received",
-        },
-        {
-          icon: "mdi-package-up",
-          title: "เบิก-จ่าย",
-          to: "/pay",
+          title: "หน้าแรก",
+          to: "/dashboard",
         },
       ],
-      admins: [
-        {
-          icon: "mdi-package-up",
-          title: "หมวดหมู่",
-          to: "/settings/categoreis",
-        },
-        {
-          icon: "mdi-package-up",
-          title: "กลุ่ม",
-          to: "/settings/group",
-        },
-        {
-          icon: "mdi-package-up",
-          title: "โซนการขาย",
-          to: "/settings/trading_zone",
-        },
-        {
-          icon: "mdi-package-up",
-          title: "ประเภทผู้จำหน่าย",
-          to: "/settings/vendor_type",
-        },
-        {
-          icon: "mdi-package-up",
-          title: "คลัง",
-          to: "/settings/warehouse",
-        },
-      ],
-      users: [
-        {
-          icon: "mdi-package-up",
-          title: "users",
-          to: "/settings/users",
-        },
-      ],
+
       title: "ระบบสต๊อกสินค้า",
     };
+  },
+
+  methods: {
+    async logout() {
+      this.$swal({
+        title: "ออกจากระบบ",
+        text: "ยืนยันเพื่อออกจากระบบ",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "ยืนยัน",
+        cancelButtonText: "ยกเลิก",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          //success
+          this.$auth.logout();
+          // this.$router.push("/login");
+        }
+      });
+    },
   },
 };
 </script>
@@ -182,4 +90,3 @@ export default {
   font-family: "Sarabun", sans-serif;
 }
 </style>
-
